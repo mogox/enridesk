79fc566621121d214968953db1b547a012349a7e
Enrique Carlos Mogollan <emogollan@gmail.com>
2015-10-11 22:53:52 -0700

    Adding filter and home controller spec

--- spec/controllers/filters_controller_spec.rb
+++ spec/controllers/filters_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe FiltersController, type: :controller do
+  describe "#show" do
+    let(:kases) { ["Case1", "Case2"] }
+    let(:filter) { "A filter" }
+    subject { get :show, id: 10 }
 
+    before do
+      expect(Filter).to receive(:all) { {} }
+      expect(Label).to receive(:all) { {} }
+    end
+
+    it "loads the filter and all the cases associated with that filter" do
+
+      expect(Filter).to receive(:find).with("10"){ filter }
+      expect(Case).to receive(:by_filter).with("10"){ kases }
+      subject
+      expect(assigns(:filter) ).to eq filter
+      expect(assigns(:cases) ).to eq kases
+    end
+  end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  describe "#index" do
+    let(:labels) { ["label1", "label2"] }
+    let(:filters) { ["filter1", "filter2"] }
 
+    it "loads the filters and the labels" do
+      expect(Filter).to receive(:all) { filters }
+      expect(Label).to receive(:all) { labels }
+      get :index
+    end
+  end
 end
